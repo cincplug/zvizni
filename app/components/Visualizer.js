ef427be55163ebd3932165a567c95d49e8086ab1
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-08-18 13:06:57 +0200

    Refactor visualizer

--- app/components/Visualizer.js
+++ app/components/Visualizer.js
@@ -25,52 +25,60 @@ const Visualizer = ({ analyser, visualizationType }) => {
 
       canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-      if (visualizationType === "stripes") {
-        const barWidth = (canvas.width / bufferLength) * 2.5;
-        let barHeight;
-        let x = 0;
+      const visualizations = {
+        stripes: () => {
+          const barWidth = (canvas.width / bufferLength) * 2.5;
+          let x = 0;
 
-        for (let i = 0; i < bufferLength; i++) {
-          barHeight = dataArray[i] * (canvas.height / 256);
-          canvasCtx.fillStyle = `rgb(${barHeight + 100}, 50, 50)`;
-          canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-          x += barWidth + 1;
-        }
-      } else if (visualizationType === "line") {
-        canvasCtx.beginPath();
-        canvasCtx.moveTo(0, canvas.height - (dataArray[0] * canvas.height) / 256);
+          dataArray.forEach((value) => {
+            const barHeight = value * (canvas.height / 256);
+            canvasCtx.fillStyle = `rgb(${barHeight + 100}, 50, 50)`;
+            canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth + 1;
+          });
+        },
+        line: () => {
+          canvasCtx.beginPath();
+          canvasCtx.moveTo(0, canvas.height - (dataArray[0] * canvas.height) / 256);
 
-        for (let i = 1; i < bufferLength; i++) {
-          const y = canvas.height - (dataArray[i] * canvas.height) / 256;
-          const x = (i / bufferLength) * canvas.width;
-          canvasCtx.lineTo(x, y);
-        }
-
-        canvasCtx.strokeStyle = "rgb(50, 50, 200)";
-        canvasCtx.lineWidth = 2;
-        canvasCtx.stroke();
-      } else if (visualizationType === "flower") {
-        const centerX = canvas.width / 2;
-        const centerY = canvas.height / 2;
-        const maxRadius = Math.min(centerX, centerY) * 4;
-
-        canvasCtx.beginPath();
-        for (let i = 0; i < bufferLength; i++) {
-          const radius = (dataArray[i] / 256) * maxRadius;
-          const angle = (i / bufferLength) * i;
-          const x = centerX + radius * Math.cos(angle);
-          const y = centerY + radius * Math.sin(angle);
-
-          if (i === 0) {
-            canvasCtx.moveTo(x, y);
-          } else {
+          dataArray.forEach((value, i) => {
+            const y = canvas.height - (value * canvas.height) / 256;
+            const x = (i / bufferLength) * canvas.width;
             canvasCtx.lineTo(x, y);
-          }
-        }
-        canvasCtx.closePath();
-        canvasCtx.strokeStyle = "rgb(50, 0, 100)";
-        canvasCtx.lineWidth = 2;
-        canvasCtx.stroke();
+          });
+
+          canvasCtx.strokeStyle = "rgb(50, 50, 200)";
+          canvasCtx.lineWidth = 2;
+          canvasCtx.stroke();
+        },
+        flower: () => {
+          const centerX = canvas.width / 2;
+          const centerY = canvas.height / 2;
+          const maxRadius = Math.min(centerX, centerY) * 4;
+
+          canvasCtx.beginPath();
+          dataArray.forEach((value, i) => {
+            const radius = (value / 256) * maxRadius;
+            const angle = (i / bufferLength) * i;
+            const x = centerX + radius * Math.cos(angle);
+            const y = centerY + radius * Math.sin(angle);
+
+            if (i === 0) {
+              canvasCtx.moveTo(x, y);
+            } else {
+              canvasCtx.lineTo(x, y);
+            }
+          });
+          canvasCtx.closePath();
+          canvasCtx.strokeStyle = "rgb(50, 0, 100)";
+          canvasCtx.lineWidth = 2;
+          canvasCtx.stroke();
+        },
+      };
+
+      const visualize = visualizations[visualizationType];
+      if (visualize) {
+        visualize();
       }
     };
 
