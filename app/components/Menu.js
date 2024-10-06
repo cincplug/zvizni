62760f37adec577e3e896da88c5ff85d61659530
Luka Stanisavljevic <luka@cincplug.com>
2024-10-06 12:12:22 +0200

    Extract analysis methods

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { settingsConfig } from "../settingsConfig";
 import MenuToggle from "./MenuToggle";
+import { startAnalyzing, stopAnalyzing } from "../utils/analysis";
 
 const visualizationModes = [
   "flower",
@@ -33,46 +34,6 @@ const Menu = ({
       ...prevSettings,
       [name]: type === "checkbox" ? e.target.checked : value
     }));
-  };
-
-  const startAnalyzing = () => {
-    audioContextRef.current = new (window.AudioContext ||
-      window.webkitAudioContext)();
-
-    navigator.mediaDevices
-      .getUserMedia({
-        audio: {
-          sampleRate: 16000,
-          channelCount: 1
-        }
-      })
-      .then((stream) => {
-        sourceRef.current =
-          audioContextRef.current.createMediaStreamSource(stream);
-
-        analyserRef.current = audioContextRef.current.createAnalyser();
-        gainNodeRef.current = audioContextRef.current.createGain();
-
-        sourceRef.current.connect(gainNodeRef.current);
-        gainNodeRef.current.connect(analyserRef.current);
-        analyserRef.current.connect(audioContextRef.current.destination);
-
-        analyserRef.current.fftSize = 1024;
-        gainNodeRef.current.gain.value = 1;
-
-        onAnalysisStateChange(true, analyserRef.current);
-      })
-      .catch((err) => {
-        console.error("Error accessing microphone:", err);
-      });
-  };
-
-  const stopAnalyzing = () => {
-    if (audioContextRef.current) {
-      audioContextRef.current.close();
-    }
-
-    onAnalysisStateChange(false, null);
   };
 
   return (
@@ -175,7 +136,7 @@ const Menu = ({
                   Save
                 </button>
                 <button
-                  onClick={stopAnalyzing}
+                  onClick={() => stopAnalyzing(onAnalysisStateChange, audioContextRef)}
                   className="p-2 rounded bg-red-500 text-white text-sm"
                 >
                   Stop
@@ -193,7 +154,7 @@ const Menu = ({
             <p className="text-sm mb-4">audio visualization prototype</p>
           </div>
           <button
-            onClick={startAnalyzing}
+            onClick={() => startAnalyzing(onAnalysisStateChange, audioContextRef, sourceRef, analyserRef, gainNodeRef)}
             className="px-4 py-2 rounded bg-blue-500 text-white w-3/4"
           >
             Start
