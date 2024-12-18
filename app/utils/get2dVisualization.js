196a372dfe68d29c522c654da14fc0566cc96e42
Luka Stanisavljevic <luka@cincplug.com>
2024-12-18 17:15:58 +0100

    Improve visualization

--- app/utils/get2dVisualization.js
+++ app/utils/get2dVisualization.js
@@ -1,5 +1,26 @@
+const createLineTo = (ctx, width, height) => {
+  return (pointX, pointY) => {
+    const x = Math.min(width, Math.max(0, pointX));
+    const y = Math.min(height, Math.max(0, pointY));
+    ctx.lineTo(x, y);
+  };
+};
+
 export const get2dVisualization = {
-  zezni: ({ ctx, i, totalPoints, size, x, y, prevX, prevY, settings }) => {
+  zezni: ({
+    ctx,
+    i,
+    totalPoints,
+    size,
+    x,
+    y,
+    width,
+    height,
+    prevX,
+    prevY,
+    settings
+  }) => {
+    const lineTo = createLineTo(ctx, width, height);
     const angle = (i / totalPoints) * settings.angleRange;
 
     const pointX = x + size * Math.cos(angle);
@@ -8,33 +29,36 @@ export const get2dVisualization = {
     const avgX = (prevX + pointX) / 2;
     const avgY = (prevY + pointY) / 2;
 
-    ctx.lineTo(avgX, avgY);
+    lineTo(pointX, pointY);
   },
   mazni: ({ ctx, i, totalPoints, size, x, y, width, height, settings }) => {
+    const lineTo = createLineTo(ctx, width, height);
     const angle = (i / totalPoints) * settings.angleRange;
 
     const sizeIncrement = y / x;
     const pointX = x + (size - i * sizeIncrement) * Math.cos(angle) - width / 2;
     const pointY = y - ((size + i / sizeIncrement) * Math.sin(angle)) / 3;
 
-    ctx.lineTo(pointX, pointY);
+    lineTo(pointX, pointY);
   },
-  lezni: ({ ctx, i, totalPoints, size, x, y, width, settings }) => {
+  lezni: ({ ctx, i, totalPoints, size, x, y, width, height, settings }) => {
+    const lineTo = createLineTo(ctx, width, height);
     const angle = (i / totalPoints) * settings.angleRange;
 
     const sizeIncrement = y / x;
     const pointX = x + ((size - i * sizeIncrement) % x);
     const pointY = y - ((size + i / sizeIncrement) * Math.sin(angle)) / 3;
 
-    ctx.lineTo(pointX, pointY);
+    lineTo(pointX, pointY);
   },
-  smazni: ({ ctx, i, totalPoints, size, x, y, width, settings }) => {
+  smazni: ({ ctx, i, totalPoints, size, x, y, width, height, settings }) => {
+    const lineTo = createLineTo(ctx, width, height);
     const angle = (i / totalPoints) * settings.angleRange;
 
     const sizeIncrement = width % x;
     const pointX = x / (size - i * sizeIncrement) + x;
     const pointY = y - (size + i / sizeIncrement) * Math.sin(angle);
 
-    ctx.lineTo(pointX, pointY);
+    lineTo(pointX, pointY);
   }
 };
